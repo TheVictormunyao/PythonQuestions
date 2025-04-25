d9b1a8f3adb1779668c478920d9995232bc65b34
Munyao Victor <munyaovictor61@gmail.com>
2025-04-26 01:59:20 +0300

    Add a for loop to numbers sum in list

---
+++ sum_list/sum_list.py
@@ -0,0 +1,12 @@
+def sum_list (numbs):
+    sum = 0
+    #for loop to iterate through the list
+    for i in numbs:
+        sum += i
+    return sum
+  
+  #Summed Output  
+if __name__ == "__main__":
+    print(sum_list([1,2,3,4,5]))    
+
+
